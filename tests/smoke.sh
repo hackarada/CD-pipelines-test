f7a56a1a542bc932f1352552b0fff3c85e8888e4
hackarada <ermi44@gmail.com>
2022-04-20 18:02:57 -0700

    change permision of shellscript

--- tests/smoke.sh
+++ tests/smoke.sh
@@ -1,6 +1,6 @@
 #Referenced and modified from https://circleci.com/blog/smoke-tests-in-cicd-pipelines/
 #!/bin/bash
-URL="http://www.goosgle.com"
+URL="http://www.google.com"
 STATUS=$(curl -o /dev/null -s -w '%{http_code}\n' $URL)
 if [ $STATUS -eq 200 ];
 then
